c59a13c9da3c6699608d0ad947a2d4791ee8c301
anatoly techtonik <techtonik@gmail.com>
2010-02-04 14:01:51 +0200

    now we want it to be available from PyPI  * add README.txt with checklist for package release    * build source and binary distributions      python setup.py sdist    * upload everything to PyPI      python upload  * add author and website url info to make `setup.py sdist` happier

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from distutils.core import setup
 setup(
     name='wget',
     version='0.1',
+    author='anatoly techtonik <techtonik@gmail.com>',
+    url='http://bitbucket.org/techtonik/python-wget/',
+
     py_modules=['wget'],
 )
